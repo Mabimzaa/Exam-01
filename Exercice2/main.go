5494741fa12736a7eabed2e178cf275b734bd2cf
Mabimzaa <leneveuflavien40@gmail.com>
2025-09-08 10:16:39 +0200

    Exercice2

--- Exercice2/main.go
+++ Exercice2/main.go
@@ -1,5 +1,20 @@
 package main
+
 import "fmt"
+
 func CountLetters(s string) int {
+	count := 0
 	for _, char := range s {
-		if char < '' || char > 'z' {
+		if (char >= 'A' && char <= 'Z') || (char >= 'a' && char <= 'z') {
+			count++
+		}
+	}
+	return count
+}
+
+func main() {
+	fmt.Println(CountLetters("Melvin, le beau gosse!"))
+	fmt.Println(CountLetters("1234!@#$"))
+	fmt.Println(CountLetters("J'aime la promo"))
+
+}
